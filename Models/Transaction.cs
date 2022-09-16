21134d7765e676d991a64aa7784c3eadb94c1509
KK6355 <kangjiawei666@126.com>
2022-09-16 15:47:49 +1200

    implement of validation for transaction form

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -7,8 +7,10 @@ namespace ExpenseTracker.Models
     {
         [Key]
         public int TransactionId { get; set; }
+        [Range(1,int.MaxValue,ErrorMessage ="Please select a category")]
         public int CategoryId { get; set; }
         public Category? Category { get; set; } //navgation property so that it knows categoryId is the FK
+        [Range(1, int.MaxValue, ErrorMessage = "Amount should be greater than 0")]
         public int Amount { get; set; }
 
         [Column(TypeName = "nvarchar(75)")]
